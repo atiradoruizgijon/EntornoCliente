bd2b0e42030f6ddff66e8dc7cea85f562ec6e809
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2025-11-01 13:54:05 +0100

    Ejercicio 4 casi terminado, falta implantar el método costeEstancia() y hacer los test con Jasmine

---
+++ Tema3_ObjetosPredefinidos/Boletin3/Ejercicio4/Reserva.js
@@ -0,0 +1,54 @@
+class Reserva {
+    _nombre;
+    _dni;
+    _fechaEntrada;
+    _fechaSalida;
+
+    constructor(nombre, dni, fechaEntrada, fechaSalida) {
+        this._nombre = nombre;
+        this._dni = dni;
+        this._fechaEntrada = new Date((fechaEntrada.split("/")[2] + "/" + fechaEntrada.split("/")[1] + "/" + fechaEntrada.split("/")[0]));
+        this._fechaSalida = new Date((fechaSalida.split("/")[2] + "/" + fechaSalida.split("/")[1] + "/" + fechaSalida.split("/")[0]));
+    }
+
+    get nombrePila() {
+        return this._nombre.split(";")[2].trim();
+    }
+    get apellido1() {
+        return this._nombre.split(";")[0].trim();
+    }
+    get apellido2() {
+        return this._nombre.split(";")[1].trim();
+    }
+
+    get fechaEntrada() {
+        return this._fechaEntrada;
+    }
+    get fechaSalida() {
+        return this._fechaSalida;
+    }
+
+    get codigoCliente() {
+        return (this.nombrePila.charAt(0) + this.apellido1 + this._dni.substring(5, 8)).toUpperCase();
+    }
+
+    get numeroDiasEstancia() {
+        // 86400000 = milisegundos en un dia.
+        return Math.abs(this._fechaEntrada - this._fechaSalida) / 86400000;
+    }
+
+    modificarFechas(fecEn, fecSa) {
+        let fec1 = new Date((fecEn.split("/")[2] + "/" + fecEn.split("/")[1] + "/" + fecEn.split("/")[0]));
+        let fec2 = new Date((fecSa.split("/")[2] + "/" + fecSa.split("/")[1] + "/" + fecSa.split("/")[0]));
+        
+        if ((fec1 - fec2) > 0) throw new Error("Fecha de salida debe de ser posterior a la de entrada.");
+        if (Math.abs(fec1 - fec2) < 86400000) throw new Error("La reserva ha de ser de mínimo un día.");
+        
+        this._fechaEntrada = fec1;
+        this._fechaSalida = fec2;
+    }
+
+    costeEstancia() {
+        
+    }
+}
