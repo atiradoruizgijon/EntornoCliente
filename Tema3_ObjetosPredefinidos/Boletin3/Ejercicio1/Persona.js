72bd27ad78eecd34a88386d65fb76b7cd8971a6a
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2025-11-01 08:51:11 +0100

    Ejercicio 1, clase Persona hecha

---
+++ Tema3_ObjetosPredefinidos/Boletin3/Ejercicio1/Persona.js
@@ -0,0 +1,51 @@
+class Persona {
+
+    _nombre;
+    _edad;
+    _altura;
+
+    constructor(nombre, edad, altura) {
+        this._nombre = nombre;
+        this._edad = edad;
+        this._altura = altura;
+    }
+
+    get nombre() {
+        return this._nombre.toUpperCase();
+    }
+    get edad() {
+        return this._edad;
+    }
+    get altura() {
+        return this._altura;
+    }
+
+    set nombre(n) {
+        if (n.trim() == "") throw new Error("El nombre no puede estar vacío.");
+        this._nombre = n;
+    }
+    set edad(n) {
+        if (n < 0) throw new Error("La edad no puede ser un número negativo");
+        this._edad = n;
+    }
+    set altura(n) {
+        if (n < 0) throw new Error("La altura no puede ser un número negativo");
+        this._altura = n;
+    }
+
+    toString() {
+        return this._nombre + ", " + this._edad + " años, altura de " + this._altura + "m";
+    }
+
+    static cmpNombre(p1, p2) {
+        return p1.nombre.localeCompare(p2.nombre);
+    }
+
+    static cmpEdad(p1, p2) {
+        return p1.edad - p2.edad;
+    }
+    
+    static cmpAltura(p1, p2) {
+        return p1.altura - p2.altura;
+    }
+}
