5d7d5def982fe0a6a0d6b08bb142a2b32a7fdf45
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2025-11-16 15:57:07 +0100

    Ejercicio 5 hecho

---
+++ Tema4_DOM/Boletin2/Ejercicio5/lotaria_primitiva.js
@@ -0,0 +1,34 @@
+function bonoloto() {
+    let array = [];
+    let aleatorio;
+
+    for (let i = 0; i < 6; i++) {
+        do {
+            aleatorio = Math.round((Math.random() * 49) + 1);
+        } while (array.includes(aleatorio));
+
+        array[i] = aleatorio;
+    }
+    return array;
+}
+
+function generar() {
+    let tabla = document.querySelector("table");
+    tabla.innerHTML = "";
+
+    tabla.insertAdjacentHTML("beforeend", "<tbody></tbody>");
+    let aleatorios = bonoloto();
+    console.log(aleatorios);
+
+    let contador = 1;
+    for (let i = 1; i < 8; i++) {
+        tabla.firstElementChild.insertAdjacentHTML("beforeend", "<tr></tr>");
+        for (let j = 1; j < 8; j++) {
+            if (aleatorios.includes(contador)) {
+                tabla.firstElementChild.lastChild.insertAdjacentHTML("beforeend", `<td class="ganador">${contador++}</td>`);
+            } else {
+                tabla.firstElementChild.lastChild.insertAdjacentHTML("beforeend", `<td>${contador++}</td>`);
+            }
+        }
+    }
+}
