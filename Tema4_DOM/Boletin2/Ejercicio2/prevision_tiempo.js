85ffcc9b5c4282a3ae0367b54224c07d8cc108f5
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2025-11-16 12:47:26 +0100

    Ejercicio 2 hecho

---
+++ Tema4_DOM/Boletin2/Ejercicio2/prevision_tiempo.js
@@ -0,0 +1,58 @@
+let encabezados = ["Horas", "Previsión", "Viento", "Velocidad", "Lluvias"];
+let datos = [
+    {
+        hora: "19:00",
+        prevision: { temperatura: "33º", icono: "sol" },
+        viento: "oeste",
+        velocidad: "15 km/h",
+        lluvias: "0 mm"
+    },
+    {
+        hora: "20:00",
+        prevision: { temperatura: "30º", icono: "sol" },
+        viento: "norte",
+        velocidad: "5 km/h",
+        lluvias: "10 mm"
+    },
+    {
+        hora: "21:00",
+        prevision: { temperatura: "28º", icono: "luna" },
+        viento: "sur",
+        velocidad: "0 km/h",
+        lluvias: "5 mm"
+    },
+
+];
+
+let tabla = document.querySelector("table");
+// inserto la fila de la cabecera:
+tabla.insertAdjacentHTML("beforeend", "<thead><tr></tr></thead>");
+
+encabezados.forEach(element => {
+    tabla.firstElementChild.firstElementChild.insertAdjacentHTML("beforeend", `<td>${element}</td>`);
+});
+// inserto el cuerpo de la tabla
+tabla.insertAdjacentHTML("beforeend", "<tbody></tbody>");
+datos.forEach(element => {
+    tabla.children[1].insertAdjacentHTML("beforeend", `<tr>
+        <td>${element.hora}</td>
+        <td>${element.prevision.temperatura}<img src='${element.prevision.icono}.png'></td>
+        <td><img src='${element.viento}.png'></td>
+        <td>${element.velocidad}</td>
+        <td>${element.lluvias}</td>
+        </tr>`);
+    });
+
+    let horas = document.querySelectorAll("td:first-child");
+    horas.forEach((element) => {
+        element.style.fontWeight = "bold";
+    });
+    
+    let lluvia = document.querySelectorAll("tbody td:last-child");
+    lluvia.forEach((element) => {
+        element.style.fontWeight = "bold";
+        element.style.color = "blue";
+    });
+
+    // encabezado en negrita:
+    tabla.firstElementChild.firstElementChild.style.fontWeight = "bold";
