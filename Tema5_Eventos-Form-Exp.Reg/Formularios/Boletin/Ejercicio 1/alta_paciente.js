d361c613a7c6c28f6da223271e8a3857e0b3af08
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2025-11-26 17:57:23 +0100

    Ejercicio 1 boletin formularios hecho

--- Tema5_Eventos-Form-Exp.Reg/Formularios/Boletin/Ejercicio 1/alta_paciente.js
+++ Tema5_Eventos-Form-Exp.Reg/Formularios/Boletin/Ejercicio 1/alta_paciente.js
@@ -1,9 +1,9 @@
 function iniciar() {
     document.querySelector("#mifumador").addEventListener("change", fumador);
     // que se desabilite si se reseta o recarga el formulario:
-    document.querySelector("#mifumador").addEventListener("reset", fumador);
     document.querySelector("#mifumador").addEventListener("load", fumador);
-
+    addEventListener("reset", fumador);
+    
     // queria hacerlo con el evento submit pero me da problemas no sé exactamente porqué
     document.querySelector("#enviar").addEventListener("click", comprobarCigarrillos);
 }
@@ -11,7 +11,7 @@ addEventListener("load", iniciar);
 
 // manejador de eventos de la casilla fumador:
 function fumador(evt) {
-    let checkbox = evt.target;
+    let checkbox = document.querySelector("#mifumador");
     
     let select = document.querySelector("#micigarrillos");
     if (checkbox.checked) {
@@ -21,13 +21,15 @@ function fumador(evt) {
         select.removeAttribute("required", "");
         select.setAttribute("disabled", "");
     }
+    if (evt.type == 'reset') {
+        select.setAttribute("disabled", "");
+    }
 }
 
 // compruebo al mandar en el formulario que se manda un valor en el select
-function comprobarCigarrillos() {
+function comprobarCigarrillos(evt) {
     if (document.querySelector("#micigarrillos").selectedIndex == 0) {
         document.querySelector("#micigarrillos").setCustomValidity("Tienes que seleccionar un número de cigarrillos");
-        evt.preventDefault();
     } else {
         document.querySelector("#micigarrillos").setCustomValidity("");
     }
--- Tema5_Eventos-Form-Exp.Reg/Formularios/Boletin/Ejercicio 1/procesar_paciente.php
+++ Tema5_Eventos-Form-Exp.Reg/Formularios/Boletin/Ejercicio 1/procesar_paciente.php
@@ -7,9 +7,20 @@
 </head>
 <body>
     <?php
-      if (isset($_REQUEST['minombre'])) {
-        echo $_REQUEST['minombre'];
-      }  
+      echo "Nombre: ".$_REQUEST['minombre']."<br>";
+      echo "Sexo: ".$_REQUEST['misexo']."<br>";
+      echo "Altura: ".$_REQUEST['mialtura']." cm <br>";
+      echo "Fecha de Nacimiento: ".$_REQUEST['mifecha']."<br>";
+      if (isset($_REQUEST['misemana']) && $_REQUEST['misemana'] != "") echo "Semana de preferencia: ".$_REQUEST['misemana']."<br>";
+      
+      if (isset($_REQUEST['mifumador']) && $_REQUEST['mifumador'] == "") {
+        echo "Fumador: Sí<br>";
+      } else {
+        echo "Fumador: No<br>";
+      }
+
+      if (isset($_REQUEST['micigarrillos'])) echo "Nº de cigarrillos: ".$_REQUEST['micigarrillos']."<br>";
+      if (isset($_REQUEST['miobservaciones']) && $_REQUEST['miobservaciones'] != "") echo "Observación: ".$_REQUEST['miobservaciones'];
     ?>
 </body>
 </html>
