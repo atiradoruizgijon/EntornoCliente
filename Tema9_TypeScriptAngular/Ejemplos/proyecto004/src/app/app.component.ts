53a807f0e51f8f6240cb770c880b5723f6ccba83
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2026-02-15 17:46:48 +0100

    Ejercicio 3, añado 2 dados más y cambio la condición de victoria proyecto004

--- Tema9_TypeScriptAngular/Ejemplos/proyecto004/src/app/app.component.ts
+++ Tema9_TypeScriptAngular/Ejemplos/proyecto004/src/app/app.component.ts
@@ -12,6 +12,9 @@ export class AppComponent implements OnInit {
   valor1:number = 1;
   valor2:number = 1;
   valor3:number = 1;
+  valor4:number = 1;
+  valor5:number = 1;
+  total:number = this.valor1 + this.valor2 + this.valor3 + this.valor4 + this.valor5;
   mensaje:string = "";
 
   generarAleatorio():number {
@@ -19,7 +22,7 @@ export class AppComponent implements OnInit {
   }
 
   comprobarJugada() {
-    if (this.valor1 == this.valor2 && this.valor1 == this.valor3) {
+    if (this.total >= 20) {
       this.mensaje = "Has ganado";
     } else {
       this.mensaje = "Sigue intentando";
@@ -34,6 +37,9 @@ export class AppComponent implements OnInit {
     this.valor1 = this.generarAleatorio();
     this.valor2 = this.generarAleatorio();
     this.valor3 = this.generarAleatorio();
+    this.valor4 = this.generarAleatorio();
+    this.valor5 = this.generarAleatorio();
+    this.total = this.valor1 + this.valor2 + this.valor3 + this.valor4 + this.valor5;
     this.comprobarJugada();
   }
 }
