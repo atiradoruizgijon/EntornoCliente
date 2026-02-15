67bf0d6ee5a1f40f97296b224d4a0ed7988f589c
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2026-02-15 21:03:24 +0100

    Ejercicio 6, añado botones de reseteo proyecto007

---
+++ Tema9_TypeScriptAngular/Ejemplos/proyecto007/src/app/selectornumerico/selectornumerico.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selectornumerico',
+  standalone: false,
+  templateUrl: './selectornumerico.component.html',
+  styleUrl: './selectornumerico.component.css'
+})
+export class SelectornumericoComponent implements OnInit {
+    @Input() minimo: number = 1;
+    @Input() maximo: number = 1;
+    actual: number = 1;
+  
+    ngOnInit() {
+      this.actual = this.minimo;
+    }
+  
+    incrementar() {
+      if (this.actual < this.maximo) this.actual++;
+    }
+  
+    decrementar() {
+      if (this.actual > this.minimo) this.actual--;
+    }  
+  
+    fijar(numero:number) {
+      if (numero >= this.minimo && numero <= this.maximo) this.actual = numero;
+    }
+
+    resetear() {
+      this.ngOnInit();
+    }
+}
