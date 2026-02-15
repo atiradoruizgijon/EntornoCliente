1c15a2508a0af3e913fc81e3af52516b11d9a304
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2026-02-15 19:39:24 +0100

    Ejercicio 4, hago cuenta atras proyecto005

--- Tema9_TypeScriptAngular/Ejemplos/proyecto005/src/app/app.component.ts
+++ Tema9_TypeScriptAngular/Ejemplos/proyecto005/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CronometroComponent } from './cronometro/cronometro.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'proyecto005';
 
+  // el decorador viewchild hace que pueda acceder a propiedades y metodos del componente hijo
+  // añado el '!' para que no marque error.
+  @ViewChild(CronometroComponent) cronometro!: CronometroComponent;
+
+  mensaje:string = "";
+
   manejadorMultiplo10(segundos:number) {
     alert("Evento" + segundos);
+  }
+
+  manejadorFinalcountdown(segundos:number) {
+    this.mensaje = "La cuenta atrás a finalizado";
+  }
+
+  reiniciar() {
+    this.mensaje = "";
+    // llamo a la funcion que reinicia el contador
+    this.cronometro.reiniciar();
   }
 }
--- Tema9_TypeScriptAngular/Ejemplos/proyecto005/src/app/cronometro/cronometro.component.ts
+++ Tema9_TypeScriptAngular/Ejemplos/proyecto005/src/app/cronometro/cronometro.component.ts
@@ -13,6 +13,7 @@ export class CronometroComponent implements OnInit {
   // evento personalizado, entre '< >' se indica lo que puede devolver
   // para que sea un evento, ponemos el decorador @Output
   @Output() multiplo10:EventEmitter<number> = new EventEmitter();
+  @Output() finalcountdown:EventEmitter<number> = new EventEmitter();
 
   // @Input() normalmente son datos que vendran del padre
   //  y @Output() que mandaremos
@@ -20,11 +21,21 @@ export class CronometroComponent implements OnInit {
   ngOnInit(): void {
     this.segundo = this.inicio;
 
-    setInterval(() => {
-      this.segundo++;
-      if (this.segundo % 10 == 0) this.multiplo10.emit(this.segundo);
+    let contador = setInterval(() => {
+      this.segundo--;
+      if (this.segundo % 10 == 0 && this.segundo != 0) this.multiplo10.emit(this.segundo);
+      if (this.segundo == 0) {
+        this.finalcountdown.emit(this.segundo);
+        clearInterval(contador);
+      }
     }, 1000);
+    
     // para lanzar el evento:
     // this.multiplo10.emit(NUMERO);
+  }
+
+  // vuelvo a llamar al la funcion ngOnInit para que vuelva a empezar.
+  reiniciar() {
+    this.ngOnInit();
   }
 }
