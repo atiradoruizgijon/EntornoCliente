8f59d6dd2c5b8579d2d0ddd684e9df7c0e4cb683
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2026-03-01 17:22:25 +0100

    Ejercicio 1 boletin 2 Angular

--- Tema9_TypeScriptAngular/boletin2/src/app/app-routing.module.ts
+++ Tema9_TypeScriptAngular/boletin2/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TragaperrasComponent } from './tragaperras/tragaperras.component';
+import { ListadoAlumnosComponent } from './listado-alumnos/listado-alumnos.component';
+import { AltaAlumnoComponent } from './alta-alumno/alta-alumno.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tragaperras',
+    component: TragaperrasComponent
+  },
+  {
+    path: 'listadoAlumnos',
+    component: ListadoAlumnosComponent
+  },
+  {
+    path: 'altaAlumnos',
+    component: AltaAlumnoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
