1368383d09277c4c4bf32c07de7ac0ab55fc8db2
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2026-03-01 17:59:58 +0100

    Clase tragaperras a medio hacer

--- Tema9_TypeScriptAngular/boletin2/src/app/tragaperras/tragaperras.component.ts
+++ Tema9_TypeScriptAngular/boletin2/src/app/tragaperras/tragaperras.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tragaperras',
@@ -7,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrl: './tragaperras.component.css'
 })
 export class TragaperrasComponent {
+  @Input() credito:number = 0;
+  figuras = [
+    "bar", "campana", "cereza", "limon", "naranja", 
+    "platano", "sandia", "siete", "uva"      
+  ];
+  resultado = this.tirarJugada();
 
+  jugar() {
+    if (this.credito < 5) {
+      alert("Sin suficiente crédito");
+    } else {
+      this.credito -= 5;
+      this.resultado = this.tirarJugada();
+      this.comprobarResultado(this.resultado);
+    }
+  }
+
+  figuraAleatoria() {
+    return this.figuras[(Math.random() * this.figuras.length) + 1];
+  }
+
+  tirarJugada() {
+    return [this.figuraAleatoria(), this.figuraAleatoria(), this.figuraAleatoria()];
+  }
+
+  // r es el array con el resultado
+  comprobarResultado(r:any) {
+    // si los tres son iguales:
+    if (r[0] == r[1] && r[0] == r[2]) {
+      // si los 3 son siete:
+      if (r[0] == "siete") this.credito += 1000;
+      else this.credito += 20;
+      // si alguno coinciden dos al menos
+    } else if (r[0] == r[1] || r[0] == r[2] || r[1] == r[2]) {
+      this.credito += 5;
+    }
+  }
 }
